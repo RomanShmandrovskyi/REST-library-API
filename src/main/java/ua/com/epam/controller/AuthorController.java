b9f35eefb8eea3d08ab23653bbf53e56433d973d
Roman <roman.shmandrovskyj.ki.2014@gmail.com>
2019-05-03 09:42:23 +0300

    - add swagger-ui

--- src/main/java/ua/com/epam/controller/AuthorController.java
+++ src/main/java/ua/com/epam/controller/AuthorController.java
@@ -1,5 +1,6 @@
 package ua.com.epam.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/library")
+@Api(value = "Author")
 public class AuthorController {
 
     @Autowired
